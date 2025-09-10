c9b2f8432fe94f083dbd18a5520a75de0dd9295a
Ylson Santos <ylsonqueiroz@gmail.com>
2025-09-09 22:43:13 -0300

    fix: correção nos nomes

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -70,7 +70,9 @@ CommandList.displayName = CommandPrimitive.List.displayName;
 const CommandEmpty = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Empty>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Empty>
->((props, ref) => <CommandPrimitive.Empty ref={ref} className="py-6 text-center text-sm" {...props} />);
+>((props, ref) => (
+  <CommandPrimitive.Empty ref={ref} className="py-6 text-center text-sm" {...props} />
+));
 
 CommandEmpty.displayName = CommandPrimitive.Empty.displayName;
 
@@ -117,7 +119,7 @@ CommandItem.displayName = CommandPrimitive.Item.displayName;
 const CommandShortcut = ({ className, ...props }: React.HTMLAttributes<HTMLSpanElement>) => {
   return <span className={cn("ml-auto text-xs tracking-widest text-muted-foreground", className)} {...props} />;
 };
-CommandShortcut.displayName = "CommandShortcut";
+CommandShortcut.displayName = "AtalhoComando";
 
 export {
   Command,
